17b965d1e8b3b3a131bfdeedd3fbad165a9caf1b
afrokick <devbyru@gmail.com>
2019-09-11 15:23:10 +0300

    Updates

---
+++ precache-manifest.89881f6b3aca364d0eb6d5fedd7c3605.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716aeb0df17a550f130f4d93f998de8b",
+    "url": "/examples/simple-chat/index.html"
+  },
+  {
+    "revision": "3dd1c5497528be239283",
+    "url": "/examples/simple-chat/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "db5cc0d099dc9a0c4efe",
+    "url": "/examples/simple-chat/static/js/2.6917e22b.chunk.js"
+  },
+  {
+    "revision": "3dd1c5497528be239283",
+    "url": "/examples/simple-chat/static/js/main.73f06b43.chunk.js"
+  },
+  {
+    "revision": "d77742a8ac3d740cf75f",
+    "url": "/examples/simple-chat/static/js/runtime~main.6d9c51b0.js"
+  }
+]);
