c884b7bbf20c0ff659bb657947c0235a3c504b34
afrokick <devbyru@gmail.com>
2019-09-11 15:27:34 +0300

    Updates

--- src/models/message.js
+++
@@ -1,8 +0,0 @@
-export class Message {
-    constructor({ nickname, text, ts, isLocal }) {
-        this.nickname = nickname;
-        this.text = text;
-        this.ts = ts;
-        this.isLocal = isLocal;
-    }
-}
